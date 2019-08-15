47187cfc954b63d2dbdfc461937174c04e9de6f0
PixelSergey <sergey.ichtchenko@gmail.com>
2019-08-15 20:18:04 +0300

    Create a basic bot to deploy on Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: chong.py
---
+++ chong.py
@@ -0,0 +1,18 @@
+import os, discord
+
+client = discord.Client()
+TOKEN = os.environ['token']
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
