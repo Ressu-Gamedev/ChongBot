d8eb6f252349cc4f9acdc33711b96f3955494f7e
PixelSergey <sergey.ichtchenko@gmail.com>
2019-08-18 12:35:36 +0300

    Create welcome command
    Roles not yet tracked

--- chong.py
+++ chong.py
@@ -1,18 +1,53 @@
-import os, discord
+import os, discord, json
+from discord.ext import commands
 
-client = discord.Client()
-TOKEN = os.environ['token']
+bot = commands.Bot(command_prefix='*')
+roles = {}
 
-@client.event
+@bot.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
+    print('Logged in as {0.user}'.format(bot))
 
-@client.event
+
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+    if message.author == bot.user:
+		return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.lower().startswith('sigma'):
+        await message.channel.send('Simba')
+		
+	await bot.process_commands(message)
 
-client.run(TOKEN)
+
+@bot.command
+async def debug(ctx):
+	global roles
+	print(roles)
+
+
+@bot.command()
+async def welcome(ctx, *, data):
+	"""Creates a welcome message that will be tracked for reactions.
+	Data must be passed in using the format:
+	{"msg":"Message to be sent", "emoji1":"role1", "emoji2":"role2", ...}"""
+
+	processed = json.loads(data)
+	await welcome = ctx.message.channel.send(processed["msg"])
+	processed.pop("msg", None)
+	for emoji in processed.keys():
+		await welcome.add_reaction(emoji)
+	
+	global roles
+	roles = processed
+	await ctx.message.delete()
+	
+
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def shutdown():
+	await bot.close()
+
+if __name__ == "__main__":
+	TOKEN = os.environ['token']
+	bot.run(TOKEN)
